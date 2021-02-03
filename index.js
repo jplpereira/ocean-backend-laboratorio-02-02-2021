5e20379a947ba67d7e1b5136965465abfc5b2d01
João Pedro Pereira <jplpereira@gmail.com>
2021-02-02 21:34:35 -0300

    Incluindo novos endpoints

--- index.js
+++ index.js
@@ -1,14 +1,56 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const { response } = require('express');
 const app = express();
 
+app.use(bodyParser.json());
+
+const port = 3000;
+
+const mensagens = [
+  "Primeira mensagem",
+  "Segunda mensagem"
+];
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('Olá, mundo!');
 });
 
+// Read all
 app.get('/mensagens', (req, res) => {
-  res.send('Exibir todas as mensagens.');
+  res.send(mensagens);
 });
 
-app.listen(3000, () => {
-  console.info('Servidor rodando em http://localhost:3000');
+// Read single
+app.get('/mensagens/:id', (req, res) => {
+  const id  = req.params.id - 1;
+  
+  const mensagem = mensagens[id];
+  
+  res.send(mensagem);
+});
+
+// Atualizar mensagem
+app.put('/mensagens/:id', (req, res) => {
+  const id  = req.params.id - 1;
+
+  const mensagem = req.body.texto;
+
+  mensagens[id] = mensagem;
+
+  res.send('Mensagem atualizada com sucesso!');
+});
+
+// Create
+app.post('/mensagens', (req, res) => {
+  const mensagem = req.body.texto;
+
+  mensagens.push(mensagem);
+
+  res.send('Mensagem criada com sucesso.');
+  
+});
+
+app.listen(port, () => {
+  console.info('Servidor rodando em http://localhost:' + port);
 });
